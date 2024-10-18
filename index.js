955c7347d2a470c059acf72925897918987babe7
Damar Adi N <damaradii.nugroho@gmail.com>
2024-10-18 20:51:19 +0700

    connect to mongo

---
+++ index.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+async function main() {
+  /**--------------- Not allowed to be edited - start - --------------------- */
+  const mongoUri = process.env.MONGODB_URI;
+  const collection = process.env.MONGODB_COLLECTION;
+
+  const args = process.argv.slice(2);
+
+  const command = args[0];
+  /**--------------- Not allowed to be edited - end - --------------------- */
+
+  // Connect to MongoDB
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  // "title": "Inception",
+  //     "year": 2010,
+  //     "genre": ["Action", "Sci-Fi", "Thriller"],
+  //     "description": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
+  //     "director": "Christopher Nolan",
+  //     "cast": ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page", "Tom Hardy"
+  // Define a schema for the collection
+  const schema = new mongoose.Schema(
+    {
+      title: String,
+      year: Number,
+      genre: [String],
+      description: String,
+      director: String,
+      cast: [String],
+    },
+    { strict: false }
+  );
+  const Model = mongoose.model(collection, schema);
+
+  switch (command) {
+    case "check-db-connection":
+      await checkConnection();
+      break;
+    // TODO: Buat logic fungsionalitas yg belum tersedia di bawah
+    default:
+      throw Error("command not found");
+  }
+
+  await mongoose.disconnect();
+  return;
+}
+
+async function checkConnection() {
+  console.log("check db connection started...");
+  try {
+    await mongoose.connection.db.admin().ping();
+    console.log("MongoDB connection is successful!");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+  }
+  console.log("check db connection ended...");
+}
+
+main();
